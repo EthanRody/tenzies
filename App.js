27e1195e072bd739b898359164dfc29d079b4fff
EthanRody <106620609+EthanRody@users.noreply.github.com>
2023-07-12 15:58:55 -0400

    project commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    const [rollCount, setRollCount] = React.useState(0)
+    const [bestCount, setBestCount] = React.useState(localStorage.getItem("best") || 999)
+    
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            if (rollCount < bestCount) {
+                setBestCount(rollCount)
+                localStorage.setItem("best", rollCount)
+            }
+        }
+    }, [dice])
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setRollCount(prevRollCount => prevRollCount+1)
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? die : generateNewDie()
+            })
+        )
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    function newGame() {
+        setRollCount(0)
+        setTenzies(false)
+        allNewDice()
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            
+            <div className="stats">
+                <h3>Roll Count: {rollCount}</h3>
+                <h3>Best: {bestCount}</h3>
+            </div>
+            
+            {tenzies ? 
+            <button className="roll-dice" onClick={newGame}>New Game</button> :
+            <button className="roll-dice" onClick={rollDice}>Roll</button>}
+        </main>
+    )
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tenzies
- tenzies project created using JavaScript with React through Scrimba course
+ tenzies project created using JavaScript with React as final project in Scrimba React course
